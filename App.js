93928587ae4885a383503f7153dc01a58ebb5553
ntorre <n.torre@e2time.com>
2020-12-02 10:49:20 +0100

    Test React Navigation signIn / signOut

--- App.js
+++ App.js
@@ -7,15 +7,31 @@
  */
 
 import 'react-native-gesture-handler';
-import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+const AuthContext = React.createContext();
+
 function HomeScreen() {
+  const {signOut} = React.useContext(AuthContext);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={signOut} title="Sign Out" />
       <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function AuthScreen() {
+  const {signIn} = React.useContext(AuthContext);
+
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Button onPress={signIn} title="Sign In" />
+      <Text>Auth Screen</Text>
     </View>
   );
 }
@@ -23,12 +39,36 @@ function HomeScreen() {
 const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (data) => {
+        setIsSignedIn(true);
+      },
+      signOut: async () => {
+        setIsSignedIn(false);
+      },
+    }),
+    [],
+  );
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Auth" component={AuthScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 };
 
